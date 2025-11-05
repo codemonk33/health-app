b75f4163168938277215390b41f8df9c4ab402aa
codemonk33 <kodarm666@gmail.com>
2025-11-05 10:12:21 +0530

    update
    update

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // Required for react-native-reanimated
+      'react-native-reanimated/plugin',
+      // Required for expo-router
+      require.resolve('expo-router/babel'),
+    ],
+  };
+};
+
+
+
+
+
+
